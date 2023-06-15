04c36eb0d764db19ed73c6c70286af2cf823b263
Süleyman Koramaz <suleymankoramaz01@gmail.com>
2023-06-15 21:14:13 +0300

    Product Basics Implemented.
    Basics of product controller and product routes implemented.

---
+++ src/controllers/productController.js
@@ -0,0 +1,23 @@
+const sql = require('mssql');
+
+exports.getAllProducts = async (req, res) => {
+  try {
+    const request = new sql.Request();
+    const result = await request.query('SELECT * FROM Products');
+    res.status(200).json(result.recordset);
+  } catch (error) {
+    res.status(500).json({ message: 'Error retrieving products' });
+  }
+};
+
+exports.addProduct = async (req, res) => {
+  //adding a product
+};
+
+exports.updateProduct = async (req, res) => {
+  //updating a product
+};
+
+exports.deleteProduct = async (req, res) => {
+  //deleting a product
+};
---
+++ src/routes/productRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const productsController = require('../controllers/productsController');
+
+const router = express.Router();
+
+router.get('/', productsController.getAllProducts);
+router.post('/', productsController.addProduct);
+router.put('/:id', productsController.updateProduct);
+router.delete('/:id', productsController.deleteProduct);
+
+module.exports = router;
