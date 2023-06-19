1d415fce02120fa573c5a019837cb99256790fa8
Süleyman Koramaz <suleymankoramaz01@gmail.com>
2023-06-20 01:56:01 +0300

    Add product implemented.
    Add product implemented and tested.

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,24 +1,73 @@
 const sql = require('mssql');
+const config = {
+    server: 'localhost',
+    database: 'MarketDB',
+    user: 'sulo',
+    password: 'Slymn1432.',
+    port: 1433,
+    options: {
+        trustedConnection: true, // For Windows authentication
+        enableArithAbort: true,
+        trustServerCertificate: true
+    },
+};
 
 exports.getAllProducts = async (req, res) => {
     try {
-        const request = new sql.Request();
-        const result = await request.query('SELECT * FROM product');
-        res.status(200).json(result.recordset);
+        var conn = new sql.ConnectionPool(config);
+        conn.connect().then(function () {
+            var request = new sql.Request(conn);
+            request.query("SELECT * FROM product").then(function (recordSet) {
+                conn.close();
+                res.status(200).json(recordSet.recordset);
+            }).catch(function (err) {
+                conn.close();
+                res.status(500).json({ message: 'Error retrieving products' });
+            });
+
+        }).catch(function (err) {
+            console.log(err);
+        });
     } 
     catch (error) {
-         res.status(500).json({ message: 'Error retrieving products' });
+        res.status(500).json({ message: 'Error retrieving products' });
     }
 };
 
 exports.addProduct = async (req, res) => {
-  //adding a product
-};
+    const id = req.query.id;
+    const name = req.query.name;
+    const stock = req.query.stock;
+    const expiration_date = req.query.expiration_date;
+
+    console.log(id);
+    console.log(name);
+    console.log(stock);
+    console.log(expiration_date);
+  
+    try {
+      var conn = new sql.ConnectionPool(config);
+      await conn.connect();
+      var request = new sql.Request(conn);
+      
+      request.input('id', sql.Int, id);
+      request.input('name', sql.NVarChar(255), name);
+      request.input('stock', sql.Int, stock);
+      request.input('expiration_date', sql.DateTime, expiration_date);
+  
+      const result = await request.query("INSERT INTO product (id, name, stock, expiration_date) VALUES (@id, @name, @stock, @expiration_date)");
+      conn.close();
+      res.status(200).json(result.recordset);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: 'Error adding product', error: error.message });
+    }
+  };
 
 exports.updateProduct = async (req, res) => {
-  //updating a product
+    //updating a product
 };
 
 exports.deleteProduct = async (req, res) => {
-  //deleting a product
+    //deleting a product
 };
